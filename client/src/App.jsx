827d5e7c5b3e3e964ef8c10af1970a0347771960
vikash yadav <vikash9412077@gmail.com>
2024-10-04 12:06:20 +0530

    changes done in authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,24 @@
-import {lazy} from "react";
+import { lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectRoute from "./components/auth/ProtectRoute";
 
-const Home = lazy(()=> import("./pages/Home"));
-const Login = lazy(()=> import("./pages/Login"));
-const Chat = lazy(()=> import("./pages/Chat"));
-const Groups = lazy(()=> import("./pages/Groups"));
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import("./pages/Groups"));
+
+let user = false;
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route element={<ProtectRoute user={user} />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat/:chatid" element={<Chat />} />
+          <Route path="/groups" element={<Groups />} />
+        </Route>
         <Route path="/login" element={<Login />} />
-        <Route path="/chat/:chatid" element={<Chat />} />
-        <Route path="/groups" element={<Groups />} />
       </Routes>
     </BrowserRouter>
   );
